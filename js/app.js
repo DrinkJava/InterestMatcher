c28cebd6a97a41ff24fd0d6904288141349c5d2e
Nishant Maniam <nishant.maniam@gmail.com>
2015-09-27 11:37:16 -0500

    r Please enter the commit message for your changes. Lines starting r Message Fix

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-angular.module('IMapp', ['postsModule']);
+angular.module('IMapp', ['postsModule', 'ngRoute']);
+
+angular.module('IMapp').config(function($routeProvider) {
+    $routeProvider.when('/postView', {
+        controller: 'PostController',
+        templateUrl: '../modules/posts/postView.html',
+    });
+});
